cb4298bf12c7e4786e85387e6d08d86c7812aa17
g33king <turhuhxckuo@gmail.com>
2018-02-26 19:45:21 +0200

    Create ActObjs.py
    where all the object classes will be.

---
+++ ActObjs.py
@@ -0,0 +1,8 @@
+class ActSprite:
+  def __init__(self, x, y, width, height, tex, func):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.tex = tex
+    self.func = func
