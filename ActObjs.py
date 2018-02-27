62afe25fadb2b0ca95eb11bf99c398cb276bcc82
g33king <turhuhxckuo@gmail.com>
2018-02-27 11:54:27 +0200

    Update ActObjs.py

--- ActObjs.py
+++ ActObjs.py
@@ -1,8 +1,25 @@
 class ActSprite:
-  def __init__(self, x, y, width, height, tex, func):
+  def __init__(self, win, x, y, width, height, tex, func):
+    self.win = win
     self.x = x
     self.y = y
     self.width = width
     self.height = height
     self.tex = tex
     self.func = func
+    
+class ActBlock:
+  def __init__(self, win, x, y, width, height, tex):
+    self.win = win
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.tex = tex
+
+class ActWindow:
+  def __init__(self, x, y, width, height):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
